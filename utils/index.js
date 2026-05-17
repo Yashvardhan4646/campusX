4dfef53eb7a629519e3a212086ab76dcda324d5d
user-synax <usersynax@gmail.com>
2026-05-17 08:17:40 +0530

    FIX: 'formatNumber' is not exported from the requested module at utils/formatters.js.

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
 export { validateEmail, validateUsername, validatePassword, sanitizeString, validateObjectId } from './validators'
-export { formatRelativeTime, formatCount, formatDate, formatNumber } from './formatters'
+export { formatRelativeTime, formatCount, formatDate } from './formatters'
 export { extractHashtags, parseContentWithMentions } from './hashtags'
 export * from './schemas'
