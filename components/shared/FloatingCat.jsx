931b3be9d1b53a33d0dfc727e2d5117c85877902
user-synax <usersynax@gmail.com>
2026-05-18 08:14:27 +0530

    Add CustomCursor component that applies cursor to body

---
+++ components/shared/FloatingCat.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { useCat } from "@/context/CatContext";
+
+export default function CustomCursor() {
+  const { cursorEnabled, cursorStyle } = useCat();
+
+  useEffect(() => {
+    if (!cursorEnabled) {
+      document.body.style.cursor = "";
+      return;
+    }
+    document.body.style.cursor = cursorStyle;
+    return () => {
+      document.body.style.cursor = "";
+    };
+  }, [cursorEnabled, cursorStyle]);
+
+  return null;
+}
